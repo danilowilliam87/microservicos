871257f6f856f146dc8c102699d7673f09b7bd22
Danilo William dos Reis Socorro <danilowilliam87@gmail.com>
2022-09-12 10:52:34 -0300

    feat:restTemplate

--- hr-payroll/src/main/java/io/wdsolutions/hrpayroll/services/PaymentServices.java
+++ hr-payroll/src/main/java/io/wdsolutions/hrpayroll/services/PaymentServices.java
@@ -1,14 +1,31 @@
 package io.wdsolutions.hrpayroll.services;
 
-import org.springframework.stereotype.Service;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import io.swdsolutions.hrentities.Worker;
 import io.wdsolutions.hrpayroll.entities.Payment;
 
 @Service
 public class PaymentServices {
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	@Value("${hr-worker.host}")
+	private String workerHost;
 
 	public Payment getPayment(long workerId, int days) {
-		return new Payment("Tio Patinha", 150.00, days);
+		Map<String, String> variables = new HashMap<>();
+		variables.put("id", ""+workerId);
+		String url = workerHost + "/workers/{id}";
+		Worker worker = restTemplate.getForObject(url, Worker.class, variables);
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 	
 }
